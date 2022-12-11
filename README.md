b382b0fcd9722777c9a1759da8265d2ed7b1c836
Varad Joshi <varadjoshi@Varads-MBP.Dlink>
2022-12-11 16:09:12 +0530

    added new repo with mult function

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a normal file, that will be pushed to github
---
+++ funcs.py
@@ -0,0 +1,9 @@
+import math
+
+def mult(*args):
+    return math.prod([*args])
+
+m = mult(10, 20, 30 ,40)
+
+def sqroot(a):
+    return math.sqrt(a)
